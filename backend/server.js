d771904d614212b83102b13a239a7aeed8d14461
Hassham Khan <hasshamkhanswabi032-ctrl@users.noreply.github.com>
2026-03-17 00:51:41 +0500

    Fix Vercel export

--- backend/server.js
+++ backend/server.js
@@ -22,3 +22,5 @@ mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/ecommerce')
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
